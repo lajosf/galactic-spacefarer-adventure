8d605bb1e65b91b87d7445e9faf983a02a2a3904
LAJOS FÜLEP <lajosfulep@icloud.com>
2025-01-27 08:48:26 +0000

    NEW: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+                cds: true,
+                SELECT: true,
+                INSERT: true,
+                UPDATE: true,
+                DELETE: true
+            }
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            'indent': ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'no-unused-vars': ['warn'],
+            'no-console': ['warn', {
+                'allow': ['log', 'warn', 'error', 'info']
+            }]
+        },
+        ignores: [
+            'gen/**',
+            'node_modules/**',
+            'coverage/**',
+            '*.cds'
+        ]
+    }
+];
